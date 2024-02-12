c20861bb1777a33fe8ccb3e6cabf9993fac174c1
mia-a-h <159729893+mia-a-h@users.noreply.github.com>
2024-02-12 13:29:15 +0200

    Add files via upload

--- assessment1.py
+++ assessment1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 """!!!!!I got my dataset from UCI, and i tried to import it to python but it didn't work. 
@@ -9,7 +10,7 @@ ds = pd.read_excel('C:\\Users\\Mia\\Desktop\\uni\\Semestre 4\\Data Analysis\\Boo
 
 print("Length of the 'Adult' DataFrame: ", ds.__len__())
 
-# Education level by age groups - Distribution
+#
 grouped_data = ds.groupby(['age', 'education_level']).size().unstack(fill_value=0)
 grouped_data.plot(kind='bar', stacked=True, figsize=(12, 8))
 plt.title('Distribution of Education Levels Among Different Age Groups')
@@ -19,12 +20,10 @@ plt.xticks(rotation=45)
 plt.legend(title='Education Level', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.show()
-
 ds_numeric = ds.select_dtypes(include='number')
 
-# Average hours per week for each marital status
+#
 avg_hours_per_week = ds.groupby('marital_status')['hours_per_week'].mean()
-
 plt.figure(figsize=(10, 6))
 avg_hours_per_week.plot(kind='bar', color='pink')
 plt.title('Average Hours per Week by Marital Status')
@@ -33,9 +32,8 @@ plt.ylabel('Average Hours per Week')
 plt.xticks(rotation=45)
 plt.show()
 
-# Correlation matrix
+#
 correlation_matrix = ds_numeric.corr()
-
 plt.figure(figsize=(10, 8))
 plt.matshow(correlation_matrix, cmap='plasma')
 plt.title('Correlation Matrix')
@@ -44,10 +42,21 @@ plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, r
 plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 plt.show()
 
-# Plot linear distribution
+#
+x_variable = 'education_number'
+y_variable = 'hours_per_week'
+x = ds[x_variable]
+y = ds[y_variable]
+m, c = np.polyfit(x, y, 1)
+regression_line = m * x + c
 plt.figure(figsize=(8, 6))
-plt.scatter(ds_numeric['hours_per_week'], ds_numeric['education_number'], color='orange')
-plt.title('Linear Distribution of Work Hours per Week vs. Education Num')
-plt.xlabel('Hours per Week')
-plt.ylabel('Education Num')
+plt.scatter(x, y, label='Data Points')
+plt.plot(x, regression_line, color='red', label='Linear Regression')
+plt.xlabel(x_variable)
+plt.ylabel(y_variable)
+plt.title(f'Linear Regression: {y_variable} vs. {x_variable}')
+plt.legend()
+plt.grid(True)
 plt.show()
+
+
